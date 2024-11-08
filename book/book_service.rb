a1a8907d372bd15a4828fac4652e56fdfe32def2
Partykub <ratchanon_ku64@live.rmutl.ac.th>
2024-11-08 16:35:05 +0700

    test_display_details_book, test_add_book, test_find_by_id, test_update_book, test_delete_book, test_list_books

--- book/book_service.rb
+++ book/book_service.rb
@@ -4,15 +4,24 @@ class BookService
     @repository = repository
   end
 
-  def add_book()
+  def add_book(id, title, author)
+    book1 = repository.find_by_id(id)
+    book = Book.new(id, title, author)
+    @repository.add(book)
   end
 
-  def update_book()
+  def update_book(id, title:nil, author:nil)
+    book = @repository.find_by_id(id)
+    book&.title = title unless title.nil?
+    book&.author = author unless author.nil?
+    book
   end
 
-  def delete_book()
+  def delete_book(id)
+    @repository.delete(id)
   end
 
   def list_books
+    @repository.all
   end
 end
